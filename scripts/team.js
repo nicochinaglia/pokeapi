b760eb907e4eb8a29a903a2a44e92c129df8c14f
Nicolas <chinaglianr@gmail.com>
2022-06-15 11:55:46 -0300

    team_page_working

--- scripts/team.js
+++ scripts/team.js
@@ -4,16 +4,18 @@ function randomInteger(min, max) {
 
 function GerarTime(){
 
-    for(i = 0; i < 7; i++){
+    for(i = 0; i < 6; i++){
         resultado = randomInteger(1, 989);
         $.ajax({ type: "GET", 
          url: "https://pokeapi.co/api/v2/pokemon/" + resultado , 
          dataType:"json",
 }).done(function(dados) {
     console.log(dados.name);
+    pokeId = dados.id;
 
     var oImg = document.createElement("img");
-    oImg.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + resultado + '.png');
+    oImg.setAttribute('src', 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/' + pokeId + '.png');
+    oImg.setAttribute('width', '200px'); 
     document.body.appendChild(oImg);
 });
 }  
